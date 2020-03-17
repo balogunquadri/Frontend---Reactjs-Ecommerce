5b12c36b9aba2366cd56276af1bd2af614737b10
Lanre <pokerkingmedia@gmail.com>
2020-03-17 15:46:50 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+// import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Component/Navbar";
+import ProductList from "./Component/Productlist";
+import Details from "./Component/details";
+import Cart from "./Component/Cart/";
+import Default from "./Component/Default";
+import Modal from "./Component/Modal";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={ProductList}></Route>
+          <Route path="/details" component={Details}></Route>
+          <Route path="/cart" component={Cart}></Route>
+          <Route component={Default}></Route>
+        </Switch>
+        <Modal />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
